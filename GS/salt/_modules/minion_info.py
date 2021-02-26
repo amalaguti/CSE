931631269eda2ef4b1fc43e4f5011d507cd72cbc
amalaguti <adrianma78@hotmail.com>
2021-02-26 19:02:08 -0300

    Update minion_info.py

--- GS/salt/_modules/minion_info.py
+++ GS/salt/_modules/minion_info.py
@@ -4,8 +4,9 @@ log = logging.getLogger(__name__)
 def show_data():
     log.info(">>>>>>>>> {0}".format(__name__))
     result = __salt__['reg.read_value']('HKLM', 'SOFTWARE\Amazon\MachineImage', vname='AMIVersion')
-    result2 = __utils__['data.get_value'](result, 'vdata')
     '''
+    result2 = __utils__['data.get_value'](result, 'vdata')
+    
     winminion01:
     |_
       ----------
