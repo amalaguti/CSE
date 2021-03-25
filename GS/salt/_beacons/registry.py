8324738d086eca1e82d1694da1cf2ee8d883c96e
amalaguti <adrianma78@hotmail.com>
2021-03-25 17:32:03 -0300

    Update registry.py

--- GS/salt/_beacons/registry.py
+++ GS/salt/_beacons/registry.py
@@ -33,19 +33,13 @@ def beacon(config):
 
         reg_entry_check = __salt__["reg.read_value"](reg_entry_config['hive'], reg_entry_config['key'], vname=reg_entry_config['vname'])
 
-        #  TODO:
-        #  CHECK BEHAVIOR/EVENT DATA WHEN ENTRY NOT FOUND
-        log.info("+1+1+1+")
         if not reg_entry_check['success'] and reg_entry_config['on_not_found'] == True:
             log.info(">>>> Beacon registry ENTRY NOT FOUND {}".format(reg_entry))
             ret_dict[reg_entry] = "NOT FOUND"
             ret_dict["reg_entry"] = reg_entry
             ret_dict["tag"] = reg_entry
-            log.info("_________ NOT FOUND {}".format(ret_dict))
             ret.append(ret_dict)
 
-        log.info("+2+2+2+")
-        #if reg_entry_config['value'] != reg_entry_check['vdata']:
         if reg_entry_check['success'] and (reg_entry_config['value'] != reg_entry_check['vdata']):
             log.info(">>>> Beacon registry {} MISMATCH {}".format(reg_entry_config['value'], reg_entry_check['vdata']))
             ret_dict[reg_entry] = reg_entry_check
