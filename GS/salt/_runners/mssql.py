ce8bbc4236490addaa49c92df8ae21363adcfae0
amalaguti <adrianma78@hotmail.com>
2021-04-28 14:35:27 -0300

    Update mssql.py

--- GS/salt/_runners/mssql.py
+++ GS/salt/_runners/mssql.py
@@ -4,6 +4,6 @@ Runner Module for
 """
 
 def tsql_query(query, **kwargs):
-    ret = __salt__['salt.cmd'](query, **kwargs)
+    ret = __salt__['salt.cmd']('mssql.tsql_query', query, **kwargs)
 
     return ret
