b3e2f26954c2a66b75820479e5b680be1cfe086a
amalaguti <adrianma78@hotmail.com>
2021-04-28 14:39:36 -0300

    Update mssql.py

--- GS/salt/_runners/mssql.py
+++ GS/salt/_runners/mssql.py
@@ -7,3 +7,9 @@ def tsql_query(query, **kwargs):
     ret = __salt__['salt.cmd']('mssql.tsql_query', query, **kwargs)
 
     return ret
+
+
+def tsql_insert(query, **kwargs):
+    ret = __salt__['salt.cmd']('mssql.tsql_insert', query, **kwargs)
+
+    return ret
