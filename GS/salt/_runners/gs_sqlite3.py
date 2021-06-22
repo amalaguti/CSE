061b3c541980a0a24bbce83fa9bb20f1d0d70c4d
amalaguti <adrianma78@hotmail.com>
2021-06-22 16:35:56 -0300

    Update gs_sqlite3.py

--- GS/salt/_runners/gs_sqlite3.py
+++ GS/salt/_runners/gs_sqlite3.py
@@ -6,3 +6,9 @@ def fetch_rows(db=None, sql=None):
     ret = __salt__['salt.cmd']('sqlite3.fetch_rows', db, sql)
 
     return ret
+
+ 
+def modify(db=None, sql=None):
+    ret = __salt__['salt.cmd']('sqlite3.modify', db, sql)
+
+    return ret
